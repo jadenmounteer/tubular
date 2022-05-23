f0922b2ff0a1141941b80d96f3201f3a771302dc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-05-23 15:07:39 -0600

    Add api tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tubular';
+  
+  ngOnInit() {
+    this.getData();
+  }
+
+  showData(data: any) {
+    console.log(data);
+  }
+
+  getData = async () => {
+    const data = await this.apiFetch('https://tubular-backend.herokuapp.com/profiles');
+    this.showData(data);
+  }
+
+  async apiFetch(url: any) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
 }
